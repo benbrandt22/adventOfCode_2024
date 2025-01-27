b241806aacdd4f48ebbb9d1aa54d7313e345fa54
Ben Brandt <benbrandt22@gmail.com>
2025-01-27 07:54:12 -0600

    working template and script

--- Core/setupNewDay.bat
+++ Core/setupNewDay.bat
@@ -1,2 +1,18 @@
-﻿:: works for now, see if we can prompt for values
-dotnet new bb.aoc.day --DayNumber 9 --Name "Ben Test" --force
+:: (re)install the current version of the template 
+cd /D "%~dp0"
+dotnet new install DayTemplate --force
+
+
+@echo off
+
+:: Prompt the user for DayNumber
+set /P DayNumber=Day Number: 
+
+:: Prompt the user for Name
+set /P Name=Challenge Name: 
+
+:: Run the dotnet command using the user-supplied values
+dotnet new bb.aoc.day --DayNumber %DayNumber% --Name "%Name%"
+
+:: pause at the end so that the user can see the output
+pause
